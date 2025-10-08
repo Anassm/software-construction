fcdff9506ad2044a26b64ee4e68b005d486264a0
YWeezy <wesleyandrade0305@gmail.com>
2025-10-08 12:17:07 +0200

    Update reservation test data and tokens
    Added uuid-based tokens and detailed user/admin data to the test fixture. Updated test_get_reservations_admin to use new fixture values for tokens and user IDs.

--- testing/test_reservations.py
+++ testing/test_reservations.py
@@ -1,19 +1,25 @@
 import pytest
 import requests
-
+import uuid
 
 @pytest.fixture
 def _data():
     return {
         'url': 'http://localhost:8000/reservations/',
         'api_key': 'a1b2c3d4e5',
+        'tokenAdmin': str(uuid.uuid4()),
+        'tokenUser': str(uuid.uuid4()),
+        'admin:': {"id":"2","username":"gijsdegraaf","password":"1b1f4e666f54b55ccd2c701ec3435dba","name":"Gijs de Graaf","email":"gijsdegraaf@hotmail.com","phone":"+310698086312","role":"ADMIN","created_at":"2017-07-10","birth_year":1951,"active":True},
+        'user': {"id":"123","username":"janedoe","password":"e99a18c428cb38d5f260853678922e03","name":"Jane Doe","email":"","phone":"","role":"USER","created_at":"2020-01-15","birth_year":1990,"active":True}
+
 
     }
 
 
+
 def test_get_reservations_admin(_data):
-    tokenAdmin = 'adminToken123'
-    id = '1'
+    tokenAdmin = _data['admin']['tokenAdmin']
+    id = _data['user']['id']
     response = requests.get(f"{_data['url']}{id}", headers={'Authorization' : tokenAdmin})
 
     status_code = response.status_code
