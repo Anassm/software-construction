6120c2f7907727cfcb20b358c3158c0bbaf4f362
mtsahinhr <1065791@hr.nl>
2025-10-27 13:06:35 +0100

    Update IVehicles.cs

--- v2/Core/Interfaces/IVehicles.cs
+++ v2/Core/Interfaces/IVehicles.cs
@@ -1,2 +1,10 @@
 namespace v2.core.Interfaces;
 
+public interface IVehicles
+{
+    public Task<List<Vehicle>> GetAllVehiclesAsync();
+    public Task<Vehicle?> GetVehicleByIdAsync(int id);
+    public Task<Vehicle> CreateVehicleAsync(Vehicle vehicle);
+    public Task<Vehicle?> UpdateVehicleAsync(int id, Vehicle vehicle);
+    public Task<bool> DeleteVehicleAsync(int id);
+}
