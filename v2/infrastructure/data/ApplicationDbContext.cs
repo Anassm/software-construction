0ec94a987942865dfeb3dea315f5d6f36b2bf6ea
jens koning <jens.koning@icloud.com>
2025-10-28 21:54:44 +0100

    services geimplementeerd

--- v2/infrastructure/data/ApplicationDbContext.cs
+++ v2/infrastructure/data/ApplicationDbContext.cs
@@ -15,7 +15,7 @@ namespace ChefServe.Infrastructure.Data
             public DbSet<User> Users { get; set; }
             public DbSet<Vehicle> Vehicles { get; set; }
             public DbSet<Session> Sessions { get; set; }
-            public DbSet<Reservation> Reservation { get; set; }
+            public DbSet<Reservation> Reservations { get; set; }
             public DbSet<Payment> Payments { get; set; }
             public DbSet<ParkingLot> ParkingLots { get; set; }
 
