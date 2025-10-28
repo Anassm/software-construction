b169264f788264bc92dfd2cd99d32471c6d96384
mtsahinhr <1065791@hr.nl>
2025-10-29 00:08:00 +0100

    Reservations toegevoegd
    Reservations toegevoegd maar omdat er missing fields zijn ga ik later verder

--- v2/Core/DTOs/ReservationDTO.cs
+++ v2/Core/DTOs/ReservationDTO.cs
@@ -1,1 +1,21 @@
 namespace v2.Core.DTOs;
+
+public class ReservationCreateRequest
+{
+    public required string LicensePlate { get; set; }
+    public required DateTime StartDate { get; set; }
+    public required DateTime EndDate { get; set; }
+    public required Guid ParkingLotId { get; set; }
+}
+
+public class ReservationResponse
+{
+    public required Guid Id { get; set; }
+    public required string LicensePlate { get; set; }
+    public required Guid ParkingLotId { get; set; }
+    public required DateTime StartDate { get; set; }
+    public required DateTime EndDate { get; set; }
+    public required string Status { get; set; }
+    public required float TotalPrice { get; set; }
+    public required DateTime CreatedAt { get; set; }
+}
