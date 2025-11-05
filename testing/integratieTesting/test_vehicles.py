e3fc2c31980b5fac37e207ef1574e11bade5b953
jenskon <jens.koning@icloud.com>
2025-11-05 15:50:29 +0100

    prayge2

--- testing/integratieTesting/test_vehicles.py
+++ testing/integratieTesting/test_vehicles.py
@@ -33,7 +33,7 @@ def _data():
 
 def register_and_login(base_url, user):
     requests.post(f"{base_url}/register", json=user)
-    r = requests.post(f"{base_url}/login", json={"email": user["email"], "password": user["password"]})
+    r = requests.post(f"{base_url}/login", json={"email": user["username"], "password": user["password"]})
     return {"Authorization": f"Bearer {r.json()['accessToken']}"}
 
 @pytest.fixture
