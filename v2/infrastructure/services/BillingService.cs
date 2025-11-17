52505e2554dcc8af0b27fe93676bfb0848bf47e2
1018045 <1018045@hr.nl>
2025-11-17 17:01:10 +0100

    Begin aan BillingService

--- v2/infrastructure/services/BillingService.cs
+++ v2/infrastructure/services/BillingService.cs
@@ -1,1 +1,18 @@
+using v2.core.Interfaces;
+using v2.Infrastructure.Data;
 namespace v2.infrastructure.Services;
+
+public class BillingService: IBilling
+{
+    private readonly ApplicationDbContext _db; 
+
+    public BillingService(ApplicationDbContext db)
+    {
+        _db = db; 
+    }
+
+    public Task<(int statusCode, object data)> GetPaymentHistoryAsync(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+}
