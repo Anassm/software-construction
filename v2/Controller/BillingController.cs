a547f8b4da2f8e82dcd90334c7d10d112c9f760e
1018045 <1018045@hr.nl>
2025-11-19 15:34:42 +0100

    endpoint gemaakt voor de method in Service
    Deze endpoint neemt de ingelogde gebruiker zn ID van de JWT token, roept de method in BillingService.cs om de betaalgeschiedenis te krijgen van de ingelogde gebruiker en returned de correcte status code en JSON response

--- v2/Controller/BillingController.cs
+++ v2/Controller/BillingController.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Security.Claims;
+using v2.core.Interfaces;
 using v2.Core.Interfaces;
 
 namespace V2.Controllers
@@ -9,6 +11,29 @@ namespace V2.Controllers
     [Route("billing")]
     public class BillingController : ControllerBase
     {
+        private readonly IBilling _billingService;
+
+        public BillingController(IBilling billingService)
+        {
+            _billingService = billingService;
+        }
+
+        [HttpGet("history")]
+        [Authorize]
+        public async Task<IActionResult> GetPaymentHistory()
+        {
+            var userIdStr = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrWhiteSpace(userIdStr))
+                return Unauthorized(new { error = "User not logged in" });
+
+            var userId = Guid.Parse(userIdStr);
+
+            var (statusCode, data) = await _billingService.GetPaymentHistoryAsync(userId);
+            return StatusCode(statusCode, data);
+
+        }
+       
+    }
         
     }
-}
+
