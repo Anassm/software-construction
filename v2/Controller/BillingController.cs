d7cf6bcc4ef3c435aaba01a8531918a2db12d892
1018045 <1018045@hr.nl>
2025-11-19 14:31:48 +0100

    Begin BillingController

---
+++ v2/Controller/BillingController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Security.Claims;
+using v2.Core.Interfaces;
+
+namespace V2.Controllers
+{
+    [ApiController]
+    [Route("billing")]
+    public class BillingController : ControllerBase
+    {
+        
+    }
+}
