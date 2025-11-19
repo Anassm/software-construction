eaa99d4b1322e607fa7bafe8f52778ab7c24a79e
mtsahinhr <1065791@hr.nl>
2025-11-19 12:27:02 +0100

    unittests added
    werken nog niet compleet

--- v2/Core/DTOs/ReservationDTO.cs
+++ v2/Core/DTOs/ReservationDTO.cs
@@ -31,16 +31,3 @@ public class ReservationUpdateRequest
     public DateTime? EndDate { get; set; }
     public Guid? ParkingLotId { get; set; }
 }
-
-public class ReservationResponse
-{
-    public required Guid Id { get; set; }
-    public required Guid VehicleId { get; set; }
-    public required string LicensePlate { get; set; }
-    public required Guid ParkingLotId { get; set; }
-    public required DateTime StartDate { get; set; }
-    public required DateTime EndDate { get; set; }
-    public required string Status { get; set; }
-    public required float TotalPrice { get; set; }
-    public required DateTime CreatedAt { get; set; }
-}
