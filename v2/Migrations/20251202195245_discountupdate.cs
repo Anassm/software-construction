5c6b4276cb3df198b55c1288fc1a967c90c08966
YWeezy <wesleyandrade0305@gmail.com>
2025-12-02 21:23:37 +0100

    Add SavedAmount to DiscountCodes table
    Introduces a new non-nullable decimal column 'SavedAmount' to the DiscountCodes table to track the total amount saved via discount codes. Includes migration and updates the model snapshot.

---
+++ v2/Migrations/20251202195245_discountupdate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace v2.Migrations
+{
+    /// <inheritdoc />
+    public partial class discountupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "SavedAmount",
+                table: "DiscountCodes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SavedAmount",
+                table: "DiscountCodes");
+        }
+    }
+}
