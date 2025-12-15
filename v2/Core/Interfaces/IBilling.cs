8bd0835a8b6aba13b693cf85c84688ba363e3228
Anass Moussadi <a.moussadi03@gmail.com>
2025-12-16 02:06:34 +0900

    monthly invoices including tests, possibly done in wrong branch

--- v2/Core/Interfaces/IBilling.cs
+++ v2/Core/Interfaces/IBilling.cs
@@ -4,11 +4,12 @@ using v2.Core.DTOs;
 
 public interface IBilling
 {
-   
+
     Task<(int statusCode, object data)> GetMyInvoiceHistoryAsync(string identityUserId);
 
     Task<(int statusCode, object data)> GetInvoiceDetailsAsync(Guid invoiceId, string identityUserId);
 
-   Task<(int statusCode, object data)> CreateBundleInvoiceAsync(CreateBundleInvoiceDto dto, string identityUserId);
+    Task<(int statusCode, object data)> CreateBundleInvoiceAsync(CreateBundleInvoiceDto dto, string identityUserId);
     Task<(int statusCode, object data)> GetUserBillingSummaryAsync(string username, string identityUserId);
+    Task<(int statusCode, object data)> GetMyMonthlyInvoiceHistoryAsync(int year, int month, string identityUserId);
 }
