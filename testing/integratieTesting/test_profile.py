aa5825519f2267f54eee121fd40b0e86f0bc5dc9
jenskon <jens.koning@icloud.com>
2026-01-23 17:25:11 +0100

    profiles tests with secrets

--- testing/integratieTesting/test_profile.py
+++ testing/integratieTesting/test_profile.py
@@ -1,44 +1,85 @@
 import pytest
 import requests
+import os
 
 @pytest.fixture
 def _data():
-    BASE = "http://localhost:8000"
-    r = requests.post(f"{BASE}/login", json={"username": "login.user", "password": "Password123."})
-    
     return {
-        "url": "http://localhost:8000/profile",
-        "user_token": r.json().get("accesstoken"),
-        "admin_token": "adminToken123"
+        "base": os.environ["BASE_URL"],
+        "users": {
+            "user_a": {
+                "email": os.environ["USER_A_EMAIL"],
+                "password": os.environ["USER_A_PASSWORD"],
+                "username": os.environ["USER_A_USERNAME"],
+                "name": os.environ["USER_A_NAME"],
+                "role": "user"
+            },
+            "user_b": {
+                "email": os.environ["USER_B_EMAIL"],
+                "password": os.environ["USER_B_PASSWORD"],
+                "username": os.environ["USER_B_USERNAME"],
+                "name": os.environ["USER_B_NAME"],
+                "role": "user"
+            },
+            "admin": {
+                "email": os.environ["ADMIN_EMAIL"],
+                "password": os.environ["ADMIN_PASSWORD"],
+                "username": os.environ["ADMIN_USERNAME"],
+                "name": os.environ["ADMIN_NAME"],
+                "role": "admin"
+            }
+        }
     }
+
+# @pytest.fixture   
+# def _data():
+#     BASE = "http://localhost:8000"
+#     r = requests.post(f"{BASE}/login", json={"username": "login.user", "password": "Password123."})
+    
+#     return {
+#         "url": "http://localhost:8000/profile",
+#         "user_token": r.json().get("accesstoken"),
+#         "admin_token": "adminToken123"
+#     }
+
+def register_and_login(base_url, user):
+    requests.post(f"{base_url}/register", json=user)
+
+    r = requests.post(
+        f"{base_url}/login",
+        json={"username": user["username"], "password": user["password"]},
+    )
+
+    if r.status_code != 200 or "accesstoken" not in r.json():
+        pytest.fail(
+            f"Fout bij inloggen: {r.status_code} - {r.text} {r.json()}"
+        )
+
+    body = r.json()
+    return body['accesstoken']
 
 @pytest.fixture
 def user_headers(_data):
-    return {"Authorization": f'Bearer {_data["user_token"]}'}
+    return {"Authorization": f'Bearer {register_and_login(_data["base"], _data["users"]["user_a"])}'}
 
 @pytest.fixture
 def admin_headers(_data):
-    return {"Authorization": f'Bearer {_data["admin_token"]}'}
+    return {"Authorization": f'Bearer {register_and_login(_data["base"], _data["users"]["admin"])}'}
 
 def test_profile_no_auth(_data):
-    r = requests.get(_data["url"])
+    r = requests.get(_data["base"] + "/profile")
     assert r.status_code in [401, 403]
 
 def test_profile_user_ok(_data, user_headers):
-    r = requests.get(_data["url"], headers=user_headers)
+    r = requests.get(_data["base"] + "/profile", headers=user_headers)
     print(f"hellolb{r.text}{user_headers}")
     assert r.status_code == 200
 
-# def test_profile_admin_ok(_data, admin_headers):
-#     r = requests.get(_data["url"], headers=admin_headers)
-#     print(f"hellol{r.text}")
-#     assert r.status_code == 200
-
 def test_profile_invalid_token(_data):
-    r = requests.get(_data["url"], headers={"Authorization": "invalid"})
+    r = requests.get(_data["base"] + "/profile", headers={"Authorization": "invalid"})
     assert r.status_code in [400, 401, 403]
 
 def test_profile_after_logout(_data, user_headers):
-    requests.get("http://localhost:8000/logout", headers=user_headers)
-    r = requests.get(_data["url"], headers=user_headers)
+    requests.get(_data["base"] + "/logout", headers=user_headers)
+    r = requests.get(_data["base"] + "/profile", headers=user_headers)
     assert r.status_code in [200, 401, 403]
